1fcc92c453a235121c168b806f0612e4233fc566
1Samper1 <quentin.arzalier@free.fr>
2024-04-17 20:25:36 +0200

    Initial Commit

---
+++ AFM_DLL/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFM_DLL
+{
+    public static class TestClass
+    {
+        public static int Return3()
+        {
+            return 3;
+        }
+    }
+}
